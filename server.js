9b8f786f3a956316d529dde297094a2d7616724d
dantheman <danogbo0@gmail.com>
2023-09-19 10:10:17 +0100

    auth token

--- server.js
+++ server.js
@@ -23,8 +23,10 @@ const posts = [
 
 
 
-app.get('/posts', (req,res) => {
-    res.json(posts);
+app.get('/posts', authenticateToken, (req,res) => {
+    res.json(posts.filter(post => post.username === req.user.name) );
+
+    // only the user thats logged in, gets the data allocarted to them
 });
 
 
@@ -44,5 +46,32 @@ app.post('/login', (req, res) => {
 
     // when trying to log in, it checks if you are the authorised user then creates a jwt for the user and stores all the necessary details in the jwt.
 })
+
+
+function authenticateToken(req, res, next){
+    // get the token, verify the token and return the user that the token belongs to
+    const authheader = req.headers['authorization'];
+    // get the token portion
+    const token = authheader && authheader.split(' ')[1];
+    // what the && does: if we have a 'authheader', return the token otherwise return null
+
+    // check if null
+    if(token == null){
+        return sendStatus(401);
+    }
+    
+
+    // verify the token
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        } else{
+            req.user = user
+            next();
+        }
+    })
+    // Bearer TOKEN
+}
+
 
 app.listen(3131);
